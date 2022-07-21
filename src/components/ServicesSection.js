ed11f5fb51681e2b88bfe1e274760f05d7576740
Mahammad Anis Vahora <anisvahora00@gmail.com>
2022-07-21 14:59:19 +0530

    6. Styled Services Section

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -6,48 +6,79 @@ import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 //Import image
 import home2 from "../img/home2.png";
+import styled from "styled-components";
+// Import styles
+import { About, Description, Image } from "../styles";
 
 const ServicesSection = () => {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <Description>
         <h2>
           High <span> quality</span> services.
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon">
               <img src={clock} />
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsom dolar sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={teamwork} />
               <h3>Teamwork</h3>
             </div>
             <p>Lorem ipsom dolar sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={diaphragm} />
               <h3>Diaphragm</h3>
             </div>
             <p>Lorem ipsom dolar sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={money} />
               <h3>Affordable</h3>
             </div>
             <p>Lorem ipsom dolar sit amet.</p>
-          </div>
-        </div>
-      </div>
-      <img src={home2} />
-    </div>
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
+        <img src={home2} alt="camera"></img>
+      </Image>
+    </Services>
   );
 };
 
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 4rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Card = styled.div`
+  flex-basis: 17rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
 export default ServicesSection;
